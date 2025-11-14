a7d0d7899f81402d33c7e0f5c280d529434a26b5
SelvaGaneshV <v.a.ganesh8@gmail.com>
2025-11-14 23:31:57 +0530

    updated deps

--- apps/web/svelte.config.js
+++ apps/web/svelte.config.js
@@ -6,9 +6,10 @@ const config = {
 	// Consult https://svelte.dev/docs/kit/integrations
 	// for more information about preprocessors
 	preprocess: vitePreprocess(),
-
+	compilerOptions: {
+		runes: true,
+	},
 	kit: {
-
 		alias: {
 			"@/*": "./path/to/lib/*",
 		},
